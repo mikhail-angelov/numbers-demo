e850edbe0e4b83e43a8d6d680e9dcef396b93dfe
Mikhail Angelov <mikhail.angelov@gmail.com>
2016-11-12 20:17:52 +0300

    initial commit

---
+++ lib.js
@@ -0,0 +1,67 @@
+function process(str, limit){
+	var numbers = convert(str);
+	if(!numbers || numbers.length < 2){
+		return ''
+	}
+	while(numbers.length > limit){
+		numbers = wrap(numbers)
+	}
+	while(numbers.length < limit){
+		numbers = extend(numbers)
+	}
+	return numbers;
+}
+
+function convert(str){
+	var letters = str.split(' ').join('').split('');
+	return letters.map(letter=>toNumber(letter)).join('');
+}
+
+function toNumber(c){
+	var a = 1072;
+	var e = 1077;
+	var yo = 1105;
+	var code = c.toLowerCase().charCodeAt(0) - a;
+	if(code <= (e-a)){
+		return code+1;
+	}else if(code == yo-a){
+		return 7;
+	}else{
+		return (code+1)%9 + 1;
+	}
+}
+
+function summ(a,b){
+	var sum = (+a) + (+b);
+	if(sum>9){
+		return summ(sum%10, Math.floor(sum/10))
+	}else{
+		return ''+sum
+	}
+}
+
+function wrap(src){
+	var numbers = src.split('');
+	var result = [];
+	for(var i =0;i<numbers.length - 1;i++){
+		result[i] = summ(numbers[i], numbers[i+1])
+	}
+	return result.join('');
+}
+
+function extend(src){
+	var numbers = src.split('');
+	var add = summ(numbers[numbers.length-1], numbers[numbers.length-2])
+	numbers.push(add);
+	return numbers.join('');
+}
+
+if(typeof module !== 'undefined'){
+	module.exports = {
+		process,
+		convert,
+		toNumber,
+		wrap,
+		extend
+	}
+}
---
+++ ui.js
@@ -0,0 +1,22 @@
+//bind
+var input = document.querySelector('#text');
+var number = document.querySelector('#number');
+var drawBtn = document.querySelector('#draw-btn');
+var draw = document.querySelector('.draw');
+var length = document.querySelector('.length');
+
+drawBtn.addEventListener("click", onDraw);
+input.addEventListener("keyup", onChange);
+length.addEventListener("change", onChange);
+
+
+function onChange(){
+	var text = input.value;
+	console.log('--',process(text, length.value), length.value)
+	number.innerHTML = process(text, length.value);
+	draw.style.display = "none";
+}
+
+function onDraw(){
+	draw.style.display = "block";
+}
