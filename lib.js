49c164cc01b3c39d4c83c29754c1aa364f6b39e8
Mikhail Angelov <mikhail.angelov@gmail.com>
2019-01-28 11:05:56 +0300

    dump

--- lib.js
+++ lib.js
@@ -1,157 +1,86 @@
-function process(str, limit) {
+function process(str, limit){
 	var numbers = convert(str);
-	if (!numbers || numbers.length < 2) {
+	if(!numbers || numbers.length < 2){
 		return ''
 	}
-	while (numbers.length > limit) {
+	while(numbers.length > limit){
 		numbers = wrap(numbers)
 	}
-	while (numbers.length < limit) {
+	while(numbers.length < limit){
 		numbers = extend(numbers)
 	}
 	return numbers;
 }
 
-function calculateMatrix(numbers) {
-	var numbers = numbers.split('');
-	var len = numbers.length;
-	var column = 0;
-	var result = [];
-	for (var i = 0; i < len; i++) {
-		result[i] = [];
-		result[len * 2 - i - 1] = [];
-		cell(result, i, 0, numbers[i], len);
-	}
-	//outer triangle
-	var i;
-	for (i = 1; i < len; i++) {
-		cell(result, 0, i, result[i][0], len);
-		for (var j = 1; j < len -column; j++) {
-			cell(result, j, i, summ(result[j][i - 1], result[j - 1][i]), len);
-		}
-		column++;
-	}
-	//inner triangle
-	column = 0;
-
-	for (var n = 0; n < len - 5; n++) {
-		var i = len - 2 - n;
-		var j = 3 + n;
-		var data = summ(result[i][j - 1], result[i - 1][j]);
-		result[i][j] = data;
-		result[len -2][i] = data;
-		result[j][len-2] = data;
-	}
-
-	if(len >6){
-		for (var n = 0; n < len - 7; n++) {
-			var i = len - 3 - n;
-			var j = 5 + n;
-			var data = summ(result[i][j - 1], result[i - 1][j]);
-			result[i][j] = data;
-			result[len -3][i] = data;
-			result[j][len-3] = data;
-		}
-	}
-
-	if(len >8){
-		for (var n = 0; n < len - 9; n++) {
-			var i = len - 4 - n;
-			var j = 7 + n;
-			var data = summ(result[i][j - 1], result[i - 1][j]);
-			result[i][j] = data;
-			result[len -4][i] = data;
-			result[j][len-4] = data;
-		}
-	}
-
-	return result;
-}
-
-function cell(result, i, j, data, len) {
-	result[i][j] = data;
-	result[2 * len - i - j - 1][j] = data;
-	result[j][2 * len - i - j - 1] = data;
-}
-
-function convert(str) {
+function convert(str){
 	var letters = str.split(' ').join('').split('');
-	return letters.map(letter => toNumber(letter)).join('');
+	return letters.map(letter=>toNumber(letter)).join('');
 }
 
-function toNumber(c) {
+function toNumber(c){
 	var a = 1072;
 	var e = 1077;
 	var yo = 1105;
 	var code = c.toLowerCase().charCodeAt(0) - a;
-	if (code <= (e - a)) {
-		return code + 1;
-	} else if (code == yo - a) {
+	if(code <= (e-a)){
+		return code+1;
+	}else if(code == yo-a){
 		return 7;
-	} else {
-		return (code + 1) % 9 + 1;
+	}else{
+		return (code+1)%9 + 1;
 	}
 }
 
-function summ(a, b) {
-	if ((+a) >= 0 && (+b) >= 0) {
-		var sum = (+a) + (+b);
-		if (sum > 9) {
-			return summ(sum % 10, Math.floor(sum / 10))
-		} else {
-			return '' + sum
-		}
-	} else {
-		return '0';
+function summ(a,b){
+	var sum = (+a) + (+b);
+	if(sum>9){
+		return summ(sum%10, Math.floor(sum/10))
+	}else{
+		return ''+sum
 	}
 }
 
-function wrap(src) {
+function wrap(src){
 	var numbers = src.split('');
 	var result = [];
-	for (var i = 0; i < numbers.length - 1; i++) {
-		result[i] = summ(numbers[i], numbers[i + 1])
+	for(var i =0;i<numbers.length - 1;i++){
+		result[i] = summ(numbers[i], numbers[i+1])
 	}
 	return result.join('');
 }
 
-function extend(src) {
+function extend(src){
 	var numbers = src.split('');
-	var add = summ(numbers[numbers.length - 1], numbers[numbers.length - 2])
+	var add = summ(numbers[numbers.length-1], numbers[numbers.length-2])
 	numbers.push(add);
 	return numbers.join('');
 }
 
-var SQRT2D2 = Math.sqrt(2) / 2 + 0.158; //this is hack to make it even
-function rectPosition(dim, i, j) {
-	var x = dim * SQRT2D2 * i;
-	var y = dim * (j + i / 2);
+var SQRT2 = Math.sqrt(2);
+function rectPosition(dim, i, j){
+	var x = dim * SQRT2 * i / 2;
+	var y = dim * (j + i/2);
 	return {
-		A: { x: x, y: y },
-		B: { x: (x + dim * SQRT2D2), y: y + dim / 2 },
-		C: { x: (x + dim * SQRT2D2), y: y + dim * 3 / 2 },
-		D: { x: x, y: y + dim }
+		A:{x:x,y:y},
+		B:{x:(x+dim*SQRT2/2),y:y+dim/2},
+		C:{x:(x+dim*SQRT2/2),y:y+dim*3/2},
+		D:{x:x,y:y+dim}
 	}
 }
 
-function drawSegment(numbers, dim, renderer) {
-	var size = (numbers.length) * 2 - 1;
-	var collumns = 0;
+function drawSegment(numbers, dim, renderer){
+	var size = numbers.length;
+	var collumns = 1;
 	var rect;
-	var matrix = calculateMatrix(numbers);
-
-	for (var i = size; i >= 0; i--) {
-		for (var j = collumns; j >= 0; j--) {
+	for(var i=size; i<1; i--){
+		for(var j=collumns; j<1; j--){
 			rect = rectPosition(dim, i, j);
-			renderer(rect, matrix[i][j]);
+			renderer(rect);
 		}
-		collumns++;
 	}
 }
 
-
-
-if (typeof module !== 'undefined') {
+if(typeof module !== 'undefined'){
 	module.exports = {
 		process,
 		convert,
@@ -159,7 +88,6 @@ if (typeof module !== 'undefined') {
 		wrap,
 		extend,
 		rectPosition,
-		drawSegment,
-		calculateMatrix
+		drawSegment
 	}
 }
--- ui.js
+++ ui.js
@@ -2,137 +2,27 @@
 var input = document.querySelector('#text');
 var number = document.querySelector('#number');
 var drawBtn = document.querySelector('#draw-btn');
-// var draw = document.querySelector('.draw');
+var draw = document.querySelector('.draw');
 var length = document.querySelector('.length');
 var canvas = document.querySelector('#canvas');
-var filters = document.querySelectorAll('.filter');
-var colors = document.querySelectorAll('.color');
-var ctx = canvas.getContext('2d');
-
-var DIM = 18;
-var model = {
-	filters : [],
-	paletteIndex: null,
-	palette: {
-		'1': ['#840d81','#6c4ba6','#407bc1','#18b5d8','#01e9f5','#840d81','#6c4ba6','#407bc1','#18b5d8','#01e9f5'],
-		'2': ['#009C56','#4DA87F','#51BC8C','#0D6B41','#FDB925','#009C56','#4DA87F','#51BC8C','#0D6B41','#FDB925'],
-		'3': ['#AF4528','#DD614A','#F48668','#FFA86B','#FFDA78','#AF4528','#DD614A','#F48668','#FFA86B','#FFDA78']
-	}
-}
 
 drawBtn.addEventListener("click", onDraw);
 input.addEventListener("keyup", onChange);
 length.addEventListener("change", onChange);
 
-filters.forEach(function(element){
-	element.addEventListener("click", onFilter);
-});
-
-colors.forEach(function(element){
-	element.addEventListener("click", onColors);
-});
-
-function onFilter(e){
-	var index = +e.target.textContent;
-	model.filters[index] = !model.filters[index];
-	if(model.filters[index]) {
-		e.target.setAttribute('class','filter filter-selected')
-	}else{
-		e.target.setAttribute('class','filter')
-	}
-	onDraw();
-}
-function onColors(e){
-	var index = +e.target.textContent;
-	if(model.paletteIndex == index) {
-		model.paletteIndex = null;
-		e.target.setAttribute('class','color')
-	}else{
-		model.paletteIndex = index
-		e.target.setAttribute('class','color color-selected')
-	}
-	onDraw();
-}
 
 function onChange(){
 	var text = input.value;
 	console.log('--',process(text, length.value), length.value)
 	number.innerHTML = process(text, length.value);
-	// draw.style.display = "none";
+	draw.style.display = "none";
 }
 
 function onDraw(){
-	var text = number.textContent;
-	ctx.setTransform(1, 0, 0, 1, 0, 0);
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	ctx.translate(canvas.width/2, 40*text.length);
-	ctx.font = "12px serif";
-	drawSegment(text, DIM, renderer);
+	// draw.style.display = "block";
+	drawSegment(text.value, length.value, renderer)
 }
 
-function renderer(rect, text){
-	var ALFA = Math.PI/3;
-	drawRect(rect,text);
-	drawText(text, rect.D.x+DIM/4, rect.D.y);
-	ctx.rotate(ALFA);
-	drawRect(rect,text);
-	drawNumber(rect,text,ALFA, {x:-DIM/2,y:DIM})
-	ctx.rotate(ALFA);
-	drawRect(rect,text);
-	drawNumber(rect,text,2*ALFA, {x:-DIM,y:DIM/4})
-	ctx.rotate(ALFA);
-	drawRect(rect,text);
-	drawNumber(rect,text,3*ALFA, {x:-DIM/2,y:-DIM/2})
-	ctx.rotate(ALFA);
-	drawRect(rect,text);
-	drawNumber(rect,text,4*ALFA, {x:DIM/4,y:-DIM/2})
-	ctx.rotate(ALFA);
-	drawRect(rect,text);
-	drawNumber(rect,text,5*ALFA, {x:DIM*3/4,y:DIM/3})
-	ctx.rotate(ALFA);
-}
-
-function drawRect(rect,text){
-	ctx.beginPath();
-	ctx.moveTo(rect.A.x, rect.A.y);
-	ctx.lineTo(rect.B.x, rect.B.y);
-	ctx.lineTo(rect.C.x, rect.C.y);
-	ctx.lineTo(rect.D.x, rect.D.y);
-	ctx.closePath();
-	if(model.paletteIndex){
-		var index = +text
-		var color = model.palette[model.paletteIndex][index];
-		ctx.fillStyle=color;
-		ctx.fill();
-		ctx.stroke();
-	}else{
-		ctx.stroke();
-	}
-}
-
-function drawNumber(rect,text,angle,fix){
-	ctx.save();
-	ctx.rotate(-angle);
-	ctx.translate(-rect.A.x, -rect.A.y);
-	ctx.translate(
-		(rect.A.x*Math.cos(angle)-rect.A.y*Math.sin(angle) + fix.x),
-	 	(rect.A.x*Math.sin(angle)+rect.A.y*Math.cos(angle) + fix.y)
-	);
- 	drawText(text, rect.A.x, rect.A.y);
-	ctx.restore();
-}
-
-function drawText(text, x, y){
-	var index = +text;
-	if(!model.filters[index]){
-		ctx.fillStyle='black';
-		ctx.fillText(text, x, y);
-	}
-}
-
-function printDiagram(){
-    var win=window.open();
-    win.document.write("<br><img src='"+canvas.toDataURL()+"'/>");
-    win.print();
-    win.location.reload();
+function renderer(rect){
+	canvas.context
 }
